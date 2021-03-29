a60a81b342dd95a8b9f9f89086f849bdfced93ca
Alireza <asgharialireza1995@gmail.com>
2021-03-29 10:28:16 +0430

    recursion function

---
+++ index.js
@@ -0,0 +1,23 @@
+let obj1 = {
+    firstName: "ali",
+    inner_object:{
+        age:20,
+        sex:"male",
+    },
+};
+let key = 0;
+function detection(object) { 
+    let len_1 = Object.keys(object);
+    let counter_2 = Object.values(object);
+    if (key <= len_1.length  && typeof counter_2[key] =="object"){
+        
+}else if(key <= len_1.length){
+    key++;
+    detection(len_1[key]);
+}
+}
+
+
+ 
+
+
